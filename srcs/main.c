153445c2f9c443160bc87af98b9e103d99d218ba
Girardin Alex <alex@m1.home>
2022-05-11 03:16:34 +0200

    norme

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: agirardi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 16:32:16 by agirardi          #+#    #+#             */
-/*   Updated: 2022/05/11 02:42:34 by agirardi         ###   ########lyon.fr   */
+/*   Updated: 2022/05/11 03:16:24 by agirardi         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,24 @@
 
 /*
 	print erreurs ?
-	check free_structs -> *check_fork & *fork
 	supprimer ft_think si juste une ligne
 	tableau de fork ne sert a rien ?
-
 	philo[i].last_meal_time = get_time(); delay ?
 */
 
 int	main(int argc, char **argv)
 {
 	t_data		data;
-	int			status;
 
 	if (!parse_args(argc, argv))
-		return (1);
+		return (0);
 	if (!ini_structs(argc, argv, &data))
 	{
 		free_struct(&data);
-		return (1);
+		return (0);
 	}
-	status = launch_threads(&data, data.thread, data.philo);
+	launch_threads(&data, data.thread, data.philo);
 	destroy_mutex(&data);
 	free_struct(&data);
-	return (status);
+	return (0);
 }
